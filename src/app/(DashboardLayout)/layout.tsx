e39d851fc0224451493e45ce0db156430da3d4f7
yeahieakhan <yeahieakhan2017@gmail.com>
2026-04-14 20:04:01 +0600

    updated

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -1,16 +1,53 @@
-const DashboardLayout = ({
+import { AppSidebar } from "@/components/modules/sidebar/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default async function DashboardLayout({
   admin,
   user,
 }: {
   admin: React.ReactNode;
   user: React.ReactNode;
-}) => {
+}) {
   return (
-    <div>
-      {admin}
-      {user}
-    </div>
-  );
-};
+    <SidebarProvider>
+      <AppSidebar userRole="" />
 
-export default DashboardLayout;
+      <SidebarInset>
+        <header className="sticky top-0 z-10 bg-background flex h-16 items-center justify-between px-4 border-b">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <Breadcrumb className="hidden sm:flex">
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  {userRole === "user" ? "User" : "Admin"}
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        <main
+          className={`p-4 relative  pt-6 min-h-[calc(100vh-4rem)] gradientBg`}
+        >
+          {user}
+          {admin}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
